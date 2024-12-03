08b80e1220d1f34c6b1a89972f4b6d1b730120ea
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-12-03 08:30:42 +0530

    Update Read.jsx

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Read = () => {
   const [datas, setDatas] = useState([]);
