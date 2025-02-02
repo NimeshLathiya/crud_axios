c0950e74b7f1a32a1bc355da798c1292e183b3f7
nimeshlathiya <nlathiya40@gmail.com>
2025-02-02 22:01:07 +0530

    Update Read.jsx

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 const Read = () => {
   const [datas, setDatas] = useState([]);
+  const [tabledark, setTabledark] = useState("");
+
 
   let res = async () => {
     let apiData = await axios.get(
